d13d56418e9a3abe86a3c9f04273d9dbf0c1c3aa
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-13 23:21:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,8 +27,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |holiday, items|
-      caps = holiday.to_s.split(" ").each do |x|
-        x.capitalize
+      caps = []
+      holiday.to_s.split(" ").each do |x|
+        caps << x.capitalize 
       end
       puts caps.join(" ") + ": #{items.to_s.capitalize}"
     end
